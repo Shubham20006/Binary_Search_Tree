bd1b9034c281eb0a9b48128cbadba140feefab4b
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-14 14:46:35 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {           
+            Console.WriteLine("Welcome to the Binary Search Tree Programs");
+        }
+    }
+}
